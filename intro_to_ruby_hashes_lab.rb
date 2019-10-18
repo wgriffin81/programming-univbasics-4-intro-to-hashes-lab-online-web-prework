13ff3cbc14652d8e0373ab400629109cd6dec4b5
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-18 14:51:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  positive_number = {
-    :id => 5,
-  }
+  {:id => 5}
 end
 
 def my_hash_creator(key, value)
