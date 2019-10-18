09813d66c42690c16d83d2ff365a4fd21af03e60
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-18 14:58:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  new_hash = Hash.new 
-  new_hash[key] = (value)
+  
 end
 
 def read_from_hash(hash, key)
