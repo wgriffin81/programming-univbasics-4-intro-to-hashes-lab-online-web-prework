7f710536ecf285088c96459ce36aa81b723913d2
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-18 14:37:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,10 +20,14 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  positive_number = {
+    :id => 5,
+  }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+
 end
 
 def read_from_hash(hash, key)
